e6b02ee5149f3938e85098100424be6f5d12cae6
Dylan Matthews <dylan.matthews@firespring.com>
2016-08-15 14:40:13 -0500

    facebook key in heroku

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
 	def create
 		user = User.from_omniauth(request.env['omniauth.auth'])
 		cookies[:user_id] = user_id
-		flash[:success] = "Hell, #{user.name}"
+		flash[:success] = "Hello, #{user.name}"
 		redirect_to root_url
 	end
 
